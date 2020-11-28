76132c3f57a915e90e0ab46edac02bdb9ddb0cf6
themu33inman <71170982+themu33inman@users.noreply.github.com>
2020-11-28 12:48:43 +0200

    Shop interface

--- Task1/GamePlay.Designer.cs
+++ Task1/GamePlay.Designer.cs
@@ -42,6 +42,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -77,7 +78,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 503);
+            this.label2.Location = new System.Drawing.Point(12, 553);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(189, 68);
             this.label2.TabIndex = 3;
@@ -103,7 +104,7 @@
             this.panel1.Controls.Add(this.label4);
             this.panel1.Location = new System.Drawing.Point(430, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(581, 664);
+            this.panel1.Size = new System.Drawing.Size(581, 714);
             this.panel1.TabIndex = 5;
             // 
             // enemiesremainingLabel
@@ -169,7 +170,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.label7.Location = new System.Drawing.Point(9, 575);
+            this.label7.Location = new System.Drawing.Point(9, 625);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(185, 102);
             this.label7.TabIndex = 6;
@@ -178,18 +179,30 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(9, 643);
+            this.label8.Location = new System.Drawing.Point(9, 693);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(320, 34);
             this.label8.TabIndex = 7;
             this.label8.Text = "Up Arrow - attack up     Down Arrow - attack down\r\nLeft Arrow - attack left   Rig" +
     "ht Arrow - attack right\r\n";
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(12, 509);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(151, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "SHIFT - OPEN STORE";
+            // 
             // GamePlay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1023, 699);
+            this.ClientSize = new System.Drawing.Size(1023, 742);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.panel1);
@@ -227,5 +240,6 @@
         private System.Windows.Forms.Label enemiesremainingLabel;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
